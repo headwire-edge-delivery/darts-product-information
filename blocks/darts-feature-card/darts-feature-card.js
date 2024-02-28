059f6f15003ba6d9579721e07d75ffbff9effd64
Fankhauser-Dominik <domi.fankhauser@hotmail.com>
2024-02-28 09:40:10 +0100

    onImageLoad added to the Darts Feature Card

--- blocks/darts-feature-card/darts-feature-card.js
+++ blocks/darts-feature-card/darts-feature-card.js
@@ -4,40 +4,51 @@ export default function generateDFC() {
   const cardImage = document.querySelector('.darts-feature-card').querySelector('div');
   cardImage.classList.add('darts-feature-card-image');
 
-  const imgWidth = document.querySelector('.darts-feature-card-image picture img').naturalWidth;
-  const imgHeight = document.querySelector('.darts-feature-card-image picture img').naturalHeight;
+  const image = document.querySelector('.darts-feature-card-image picture img');
 
-  const screenWidth = window.innerWidth;
-  const calculatedImgWidth = screenWidth > imgWidth ? imgWidth : screenWidth - 64;
-  const calculatedImgHeight =
-    screenWidth > imgWidth ? imgHeight : imgHeight * (screenWidth / imgWidth);
+  function onImageLoad() {
+    const imgWidth = image.naturalWidth;
+    const imgHeight = image.naturalHeight;
 
-  const { children } = document.querySelector('.darts-feature-card');
-  for (let i = 1; i < children.length; i += 1) {
-    children[i].classList.add(
-      `darts-feature-item-${children[i].children[0].textContent.toLowerCase()}`,
-    );
-  }
+    const screenWidth = window.innerWidth;
+    const calculatedImgWidth = screenWidth > imgWidth ? imgWidth : screenWidth - 64;
+    const calculatedImgHeight =
+      screenWidth > imgWidth ? imgHeight : imgHeight * (screenWidth / imgWidth);
 
-  const featureItems = document.querySelectorAll('[class^="darts-feature-item-"]');
-  const itemContainer = document.createElement('div');
-  featureItems.forEach((item) => {
-    itemContainer.classList.add('darts-feature-item-container');
-    item.parentNode.insertBefore(itemContainer, item);
-    itemContainer.appendChild(item);
-  });
+    const { children } = document.querySelector('.darts-feature-card');
+    for (let i = 1; i < children.length; i += 1) {
+      children[i].classList.add(
+        `darts-feature-item-${children[i].children[0].textContent.toLowerCase()}`,
+      );
+    }
 
-  if (window.innerWidth > 1100) {
-    // horizontal View
-    itemContainer.childNodes.forEach((item) => {
-      item.style.marginTop = `-${calculatedImgHeight}px`;
+    const featureItems = document.querySelectorAll('[class^="darts-feature-item-"]');
+    const itemContainer = document.createElement('div');
+    featureItems.forEach((item) => {
+      itemContainer.classList.add('darts-feature-item-container');
+      item.parentNode.insertBefore(itemContainer, item);
+      itemContainer.appendChild(item);
     });
 
-    document.querySelector('.darts-feature-card').style.maxWidth = `${imgWidth}px`;
-    itemContainer.style.marginLeft = `${100 / (itemContainer.children.length + 1)}%`;
+    if (window.innerWidth > 1100) {
+      // horizontal View
+      itemContainer.childNodes.forEach((item) => {
+        item.style.marginTop = `-${calculatedImgHeight}px`;
+      });
+
+      document.querySelector('.darts-feature-card').style.maxWidth = `${imgWidth}px`;
+      itemContainer.style.marginLeft = `${100 / (itemContainer.children.length + 1)}%`;
+    } else {
+      // vertical View
+      document.querySelector('.darts-feature-card').style.height = `${calculatedImgWidth}px`;
+      cardImage.style.marginTop = `${(calculatedImgWidth / 2) * 0.85}px`;
+    }
+  }
+
+  // Check if the image is already loaded (for cached images) else wait for the image to be loaded
+  if (image.complete && image.naturalHeight !== 0) {
+    onImageLoad();
   } else {
-    // vertical View
-    document.querySelector('.darts-feature-card').style.height = `${calculatedImgWidth}px`;
-    cardImage.style.marginTop = `${(calculatedImgWidth / 2) * 0.85}px`;
+    image.addEventListener('load', onImageLoad);
   }
 }
